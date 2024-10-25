9533ad5194d8d598356ae16f119417222a405694
Parker Hammond <339720419@GoTVDSB.ca>
2024-10-25 08:45:16 -0400

    Add project files.

---
+++ 7.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what would u like em to count to");
+            Console.WriteLine(": ");
+            String message = Console.ReadLine();
+            for (int i = 1; i <= 5; i = i + 1)
+            {
+                Console.WriteLine(i + ". " + message);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}

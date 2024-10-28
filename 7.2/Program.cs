cbb6e3f1972584bd9f414d5c9dc87340103b764b
Parker Hammond <339720419@GoTVDSB.ca>
2024-10-28 08:52:34 -0400

    e4ds

--- 7.2/Program.cs
+++ 7.2/Program.cs
@@ -10,14 +10,132 @@ namespace _7._2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("what would u like em to count to");
-            Console.WriteLine(": ");
-            String message = Console.ReadLine();
-            for (int i = 1; i <= 5; i = i + 1)
+            int number;
+            int number2;
+            int number3;
+            //Console.WriteLine("what 3 NUMBERS WOUL U LIKE  ");
+            //Console.WriteLine(": ");
+            //number  = int.Parse(Console.ReadLine());
+            //Console.WriteLine("second number");
+            //number2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("third number");
+            //number3 = int.Parse(Console.ReadLine());
+
+
+            //for (int i = number; i <= number2; i = i + number3)
             {
-                Console.WriteLine(i + ". " + message);
+
             }
-            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+            // Console.WriteLine("countdown");
+            // for (int i = 10; i >= 0; --i)
+            {
+
+                //Console.WriteLine(i);
+
+
+            }
+
+
+
+
+
+            //Console.ReadLine();
+
+
+
+
+            //Console.WriteLine("countdown");
+            //for (int i = -10; i < 12; i += 2)
+            {
+
+              //  Console.Write(i); Console.Write("/"); Console.WriteLine(i * i);
+
+
+
+
+            }
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+            //Console.ReadLine();
+
+
+
+            string name;
+            Console.WriteLine("enter your name");
+            name = Console.ReadLine();
+            if (name == "aldworth")
+            {
+
+                for (int i = 1; i <= 5; i = i + 1)
+                {
+
+
+
+                    Console.WriteLine( i + "." + name);
+
+
+
+                }
+                Console.ReadLine();
+
+
+
+
+
+
+            }
+            else
+            {
+                for (int i = 1; i <= 10; i = i + 1)
+                {
+
+
+
+                    Console.WriteLine( i + ". "  + name);
+
+
+
+                }
+                Console.ReadLine();
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
 
         }
     }
